c7a5bd2f0a347b27d6d62216eadd52e4f9190a59
Manu <manu@gmail.com>
2025-02-08 17:08:38 +0530

    feat : added priority in product model

--- Store/models.py
+++ Store/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=500, default='', blank=True, null=True)
     image = models.ImageField(upload_to="uploads/product/")
+    priority = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
